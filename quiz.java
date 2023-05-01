b01b0ff0eab3839ba0ea618487f67ac3494b20f8
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-01 14:32:05 +0300

    delete old files and add the quiz file

---
+++ quiz.java
@@ -0,0 +1,22 @@
+    public static void main(String args[]) {
+        
+        String star = "*";
+        int j = 3 ;
+        
+        for(int i = 0 ; i < 9 ; i++){
+            
+            if(i < 4){
+                
+                System.out.println(star);
+                star += "*";
+                
+            } else if(j > 0) {
+                
+                System.out.println(star.substring(star.length() - j));
+                j--;
+                
+            }
+            
+        }
+
+    }
